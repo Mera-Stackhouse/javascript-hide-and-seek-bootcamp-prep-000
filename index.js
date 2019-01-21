69b80f40a9498f0dd19f985d75ee7b9cf6f63baf
Mera Stackhouse <mera.stackhouse@gmail.com>
2019-01-21 04:39:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector("#nested").querySelector(".target");
+}
+
+function increaseRankBy(n) {
+  var x = parseInt(n); 
+  var nodeList = document.querySelectorAll(".ranked-list");
+  var firstNodeChildren = nodeList[0].children;
+  for (var i=0; i< firstNodeChildren.length ; i++) {
+    firstNodeChildren[i].innerHTML = parseInt
+    (firstNodeChildren[i].innerHTML) + x;
+  }
+  var secondNodeChildren = nodeList[1].children;
+  for (var j=0; j< secondNodeChildren.length; j++) {
+    secondNodeChildren[j].innerHTML = parseInt(secondNodeChildren[j].innerHTML) + x;
+  }
+}
+
+function deepestChild() {
+ var targetNode = document.querySelector("#grand-node div div div div");
+ return targetNode;
+}
